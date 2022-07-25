13bfeee02d26e6e80a0146f9853b26d45b781872
colins1 <maxrozhkov1@gmail.com>
2022-07-25 20:42:02 +0300

    add

---
+++ src/js/Team.js
@@ -0,0 +1,29 @@
+export default class Team {
+    constructor() {
+      this.members = new Set();
+    }
+  
+    add(character) {
+      if (this.members.has(character)) {
+        throw new Error('Персонаж уже добавлен в команду');
+      }
+  
+      this.members.add(character);
+    }
+  
+    addAll(...characters) {
+      characters.forEach((character) => this.members.add(character));
+    }
+  
+    toArray() {
+      const array = [];
+      this.members.forEach((elem) => { array.push(elem); });
+      return array;
+    }
+
+    *[Symbol.iterator]() {
+      for (const value of this.members) {
+        yield value;
+      }
+    }
+  }
